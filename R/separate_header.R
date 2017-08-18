327151135f1b792329626333e8c0e41bbbdb8b97
R. Mark Sharp <msharp@TxBiomed.org>
2017-08-18 17:13:18 -0500

    tokenizing lines

--- R/separate_header.R
+++ R/separate_header.R
@@ -1,25 +1,30 @@
 lines <- readLines(con = "../data/lowest3.csv", skipNul = TRUE)
-lines <- lines[!lines == ""]
+tokenize_lines <- function(lines) {
+  lines <- lines[!lines == ""]
+  tokenized_lines <- vector(mode = "list", length = length(lines))
+  for (i in seq_along(lines)) {
+    tokenized_lines[[i]] <- 
+      stri_trim_both(stri_split_fixed(lines[i], ",")[[1]])
+  }
+  tokenized_lines
+}
 #' @import stringi
 #' @export
 separate_header <- function(lines) {
   ptr <- 1
-  tokens <- stri_trim_both(stri_split_fixed(lines[ptr], ",")[[1]])
-  count_empties <- length(tokens[tokens == ""])
+  count_empties <- length(lines[[ptr]][lines[[ptr]] == ""])
   header <- list(header1 = character(0), header2 = character(0))
-  header[[ptr]] <- tokens
+  header[[ptr]] <- lines[[ptr]]
   if (count_empties > 0) { # Need another line for the header
     ptr <- ptr + 1
-    tokens <- stri_trim_both(stri_split_fixed(lines[ptr], ",")[[1]])
-    header[[ptr]] <- tokens
+    header[[ptr]] <- lines[[ptr]]
   }
-  if (tokens[length(tokens)] == "") {
+  if (lines[[ptr]][length(lines[[ptr]])] == "") {
     for (i in 1:ptr) {
       header[[i]] <- header[[i]][1:(length(header[[i]]) - 1)]
     }
-    tokenized_lines <- vector(mode = "list", length = length(lines) - ptr)
     for (i in seq_along(lines)) {
-      tokenized_lines[i] <- stri_trim_both(stri_split_fixed(lines[ptr], ",")[[1]])
+      lines[[i]] <- lines[[i]][1:(length(lines[[i]]) - 1)]
     }
   }
   header <- header[1:ptr]
