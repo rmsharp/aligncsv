55178b049bf9c7e300087172c93daeb255082d3e
rmsharp <rmsharp@me.com>
2017-08-20 23:59:58 -0500

    Testing script

--- R/test.R
+++ R/test.R
@@ -1,6 +1,14 @@
-library(stringi)
-library(readr)
+library("stringi")
+library("readr")
+source("get_settings.R")
+source("separate_header.R")
+source("extract_command_arg.R")
 test <- get_settings()
-lines <- read.csv(file = "../data/lowest3.csv", header = FALSE, 
+cat(dput(test, file = ""))
+# lines <- read.csv(file = "../data/lowest3.csv", header = FALSE, 
+#                   stringsAsFactors = FALSE)
+path <- "../data/"
+lines <- read.csv(file = stri_c(path, test$filenames[1]), header = FALSE, 
                   stringsAsFactors = FALSE)
 head_and_data <- separate_header(lines, remove_trailers = TRUE)
+cat(names(head_and_data$data))
