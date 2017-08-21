0e500e9522b20475761ed3b87d836bf7996970e5
rmsharp <rmsharp@me.com>
2017-08-20 21:49:52 -0500

    starting to figure out command line management

--- R/test.R
+++ R/test.R
@@ -1,5 +1,6 @@
 library(stringi)
 library(readr)
+test <- get_settings()
 lines <- read.csv(file = "../data/lowest3.csv", header = FALSE, 
                   stringsAsFactors = FALSE)
 head_and_data <- separate_header(lines, remove_trailers = TRUE)
